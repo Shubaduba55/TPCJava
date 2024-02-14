697c9eb40c25302091507badc6b67de0a6429d03
Bovvan <shamkov2323@gmail.com>
2024-02-14 12:49:51 +0200

    Task 2 fixed interrupt of the BallThread.
    BallThread ends its work when method run() finishes. So I avoided
    calling interrupt() method, as it doesn't end the work of the
    BallThread properly. In previous version it threw InterruptedException
    in the sleep() method.

--- TPC1/src/BallThread.java
+++ TPC1/src/BallThread.java
@@ -7,18 +7,24 @@ public class BallThread extends Thread {
     @Override
     public void run(){
         try{
-            for(int i=1; i<10000; i++){
+            // Interrupt if Ball is in Basket
+            for(int i=1; i<10000 && !b.isInBasket(); i++){
                 b.move();
-
-                // Interrupt if Ball is in Basket
-                if (b.isInBasket())
-                    this.interrupt();
 
                 System.out.println("Thread name = "
                         + Thread.currentThread().getName());
+
+                // Previously, with if (b.isInBasket()) this.interrupt()
+                // We set interrupt flag as true. But the Thread was not
+                // terminated immediately. Instead, we still printed text
+                // and started sleep() of the thread. In the method sleep
+                // JVM? checks whether this thread is interrupted. And if so
+                // it throws InterruptedException.
+
                 Thread.sleep(5);
             }
         } catch(InterruptedException ignored){
+            System.out.println("INTERRUPTED EXCEPTION!!!");
         }
     }
 }
