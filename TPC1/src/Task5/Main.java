55ec47cb3fd15b8deae5904ed3da11a7dfc00563
Bovvan <shamkov2323@gmail.com>
2024-02-19 17:59:01 +0200

    Task 5 part 2 done

--- TPC1/src/Task5/Main.java
+++ TPC1/src/Task5/Main.java
@@ -2,9 +2,23 @@ package Task5;
 
 public class Main {
     public static void main(String[] args){
+        // printAsync();
+        printSync();
+    }
+
+    private static void printAsync(){
         int iterations = 5000;
         Thread A = new PrintThread('-', iterations);
         Thread B = new PrintThread('|', iterations);
+
+        A.start();
+        B.start();
+    }
+
+    private static void printSync(){
+        SyncedPrinter printer = new SyncedPrinter();
+        Thread A = new SyncPrintThread(printer,'-', true);
+        Thread B = new SyncPrintThread(printer,'|', false);
 
         A.start();
         B.start();
