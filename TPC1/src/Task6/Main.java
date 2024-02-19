61eab107b0b5e1ac9d7468e29f844166dd7c58fa
Bovvan <shamkov2323@gmail.com>
2024-02-19 18:00:00 +0200

    Task 6 part 2 done

--- TPC1/src/Task6/Main.java
+++ TPC1/src/Task6/Main.java
@@ -2,6 +2,11 @@ package Task6;
 
 public class Main {
     public static void main(String[] args){
+        syncThreadsDemo(4, 10000);
+        // twoAsyncThreads();
+    }
+
+    private static void  twoAsyncThreads(){
         int iterations = 100000;
         Counter counter = new Counter();
 
@@ -9,7 +14,7 @@ public class Main {
             @Override
             public void run() {
                 for(int i = 0; i < iterations ;i++){
-                    counter.increment();
+                     counter.increment();
                 }
             }
         });
@@ -29,8 +34,49 @@ public class Main {
         try{
             incThread.join();
             decThread.join();
-        }catch (InterruptedException e){}
+        }catch (InterruptedException ignored){}
 
-        counter.print();
+        counter.printValue();
+    }
+
+    private static void syncThreadsDemo(int numberOfThreads, int iterations){
+        Counter counter = new Counter();
+
+        Thread[] threads = new Thread[numberOfThreads];
+        int half = numberOfThreads / 2;
+        for(int i = 0; i < half; i++){
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i = 0; i < iterations; i++)
+                        //counter.syncMethodIncrement();
+                        // counter.syncBlockIncrement();
+                        counter.syncBlockObjectIncrement();
+                }
+            });
+        }
+
+        for(int i = half; i < numberOfThreads; i++){
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for(int i = 0; i < iterations; i++)
+                        // counter.syncMethodDecrement();
+                        counter.syncBlockDecrement();
+                        // counter.syncBlockObjectDecrement();
+                }
+            });
+        }
+
+        for(Thread thread : threads)
+            thread.start();
+
+       try{
+           for(Thread thread : threads)
+               thread.join();
+       }catch (InterruptedException ignored){}
+
+        counter.printValue();
+        counter.printAsyncValue();
     }
 }
